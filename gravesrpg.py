3c53a216865524e764280b4ec30d140ace06bc33
Garrett de Bakker <garrett.debakker@gmail.com>
2016-02-06 18:44:11 -0500

    Add dungeon generation system
    Add a center function to Rect to find the center of the room.
    Add an intersect function to Rect to check if it overlaps with another
    Rect.
    Add dungeon generation code to make_map() function to generate a bunch
    of rooms and connect them with tunnels.

--- gravesrpg.py
+++ gravesrpg.py
@@ -4,6 +4,9 @@ SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 MAP_WIDTH = 80
 MAP_HEIGHT = 45
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
 
 color_dark_wall = libtcod.Color(0, 0, 100)
 color_dark_ground = libtcod.Color(50, 50, 150)
@@ -50,25 +53,71 @@ class Rect:
         self.y1 = y
         self.x2 = x + w
         self.y2 = y + h
+    
+    def center(self):
+        center_x = (self.x1 + self.x2) / 2
+        center_y = (self.y1 + self.y2) / 2
+        return (center_x, center_y)
+    
+    def intersect(self, other):
+        #Check if this and another Rect intersect
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+                self.y1 <= other.y2 and self.y2 >= other.y1)
         
         
         
 def make_map():
-    global map
+    global map, player
     
     #Fill map with blocked tiles
     map = [[ Tile(True)
         for y in range(MAP_HEIGHT) ]
             for x in range(MAP_WIDTH) ]
     
-    #Create a couple rooms
-    room1 = Rect(20, 15, 10, 15)
-    room2 = Rect(50, 15, 10, 15)
-    create_room(room1)
-    create_room(room2)
+    #Dungeon generation
+    rooms = []
+    num_rooms = 0
     
-    #Create a hallway connecting the rooms
-    create_h_tunnel(25, 55, 23)
+    for r in range(MAX_ROOMS):
+        w = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        h = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        x = libtcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
+        y = libtcod.random_get_int(0, 0, MAP_HEIGHT - h - 1)
+        
+        new_room = Rect(x, y, w, h)
+        
+        #Check if the room intersects with any others
+        failed = False
+        for other_room in rooms:
+            if new_room.intersect(other_room):
+                failed = True
+                break
+        
+        if not failed:
+            create_room(new_room)
+            (new_x, new_y) = new_room.center()
+            
+            if num_rooms == 0:
+                #Spawn the player in the center of the first room
+                player.x = new_x
+                player.y = new_y
+            else:
+                #Connect subsequent rooms to the previous with tunnels
+                
+                #Center coordinates of previous room
+                (prev_x, prev_y) = rooms[num_rooms - 1].center()
+                
+                if libtcod.random_get_int(0, 0, 1) == 1:
+                    #First go horizontally, then vertically
+                    create_h_tunnel(prev_x, new_x, prev_y)
+                    create_v_tunnel(prev_y, new_y, new_x)
+                else:
+                    #First go vertically, then horizontally
+                    create_v_tunnel(prev_y, new_y, new_x)
+                    create_h_tunnel(prev_x, new_x, prev_y)
+            
+            rooms.append(new_room)
+            num_rooms += 1
         
         
         
@@ -149,12 +198,10 @@ libtcod.console_set_custom_font('dejavu10x10_gs_tc.png', libtcod.FONT_TYPE_GREYS
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'GravesRPG', False)
 con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 
-make_map()
-
 player = Object(25, 23, '@', libtcod.white)
-npc = Object(23, 23, '@', libtcod.yellow)
+objects = [player]
 
-objects = [npc, player]
+make_map()
 
 while not libtcod.console_is_window_closed():
 
